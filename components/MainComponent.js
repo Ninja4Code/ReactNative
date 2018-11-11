4572b718cae6522aa1bee5315156e4903d9f1ffc
Charles Owen <owen.charlesr@hotmail.com>
2018-11-11 18:02:24 -0500

    React Navigation

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,10 +2,29 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
 import Dishdetail from './DishdetailComponent';
-import { View } from 'react-native';
+import { View, Platform } from 'react-native';
+import {reactStackNavigator} from 'react-navigation';
+
+const MenuNavigator = createStackNavigator({
+  Menu: { screen: Menu },
+  Dishdetail: { screen: Dishdetail }
+},
+{
+  initialRouteName: 'Menu',
+  navigationOptions: {
+      headerStyle: {
+          backgroundColor: "#512DA8"
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+          color: "#fff"            
+      }
+  }
+}
+);
 
 class Main extends Component {
-  constructor(props) {
+  /*constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
@@ -14,12 +33,13 @@ class Main extends Component {
   }
   onDishSelect(dishId) {
     this.setState({selectedDish: dishId})
-  }
+  }*/
   render() { 
     return (
-      <View style={{flex:1}}>
-        <Menu dishes={this.state.dishes} onPress={(dishId) => this.onDishSelect(dishId)} />
-        <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+      <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+       {/* <Menu dishes={this.state.dishes} onPress={(dishId) => this.onDishSelect(dishId)} />
+        <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />*/}
+        <MenuNavigator />
       </View>
     );
   }
--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,32 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { View, FlatList, Image } from 'react-native';
 import { ListItem, Avatar } from 'react-native-elements';
+import {DISHES} from '../shared/dishes';
 
-function Menu(props) {
+//function Menu(props) {
+class Menu extends Component {
 
-    const renderMenuItem = ({item, index}) => {
+    constructor(props){
+        super(props);
+        this.state = {
+            dishes: DISHES
+        }
+    }
+    static navigationOptions = {
+        title: 'Menu'
+    };    
+    render() {
+        const renderMenuItem = ({item, index}) => {
 
+            return (
+                    <ListItem
+                    key={index}
+                    title={item.name}                    
+                    subtitle={item.description}   
+                    onPress ={()=> navigate('Dishdetail', {dishId:item.id})}             
+                   avatar={<Image resizeMode='cover' borderRadius={15} style={{ height:30, width:30}} source={require('./images/uthappizza.png')}/>}
+                      />
+            );
+        };
+        const {navigate} = this.props.navigation;
         return (
-            <ListItem
-                key={index}
-                title={item.name}                    
-                subtitle={item.description}  
-                onPress={() => props.onPress(item.id)}              
-                avatar={<Image resizeMode='cover' borderRadius={15} 
-                style={{ height:30, width:30}} source={require('./images/uthappizza.png')}/>}
-            />
-        );
-    };
-
-    return (
             <FlatList 
                 style={{marginTop:25}}
-                data={props.dishes}
+                data={this.state.dishes}
                 renderItem={renderMenuItem}
                 keyExtractor={item => item.id.toString()}
                 />
-    );
+       );
+    }    
 }
-
-
 export default Menu;

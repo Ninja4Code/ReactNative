9a06fe1ea50bff53a0c99674e307678fc1009e68
Charles Owen <owen.charlesr@hotmail.com>
2018-11-10 21:51:37 -0500

    React Components 1 Revised

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, FlatList } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import { ListItem, Avatar } from 'react-native-elements';
 
 function Menu(props) {
 
@@ -8,17 +8,17 @@ function Menu(props) {
 
         return (
                 <ListItem
-                    key={index}
-                    title={item.name}
-                    subtitle={item.description}
-                    hideChevron={true}
-                    leftAvatar={{ source: require('./images/uthappizza.png')}}
+                key={index}
+                title={item.name}                    
+                subtitle={item.description}                
+                avatar={<Image style={{borderRadius:50, height:50, width:50}} source={require('./images/uthappizza.png')}/>}
                   />
         );
     };
 
     return (
             <FlatList 
+                style={{marginTop:25}}
                 data={props.dishes}
                 renderItem={renderMenuItem}
                 keyExtractor={item => item.id.toString()}

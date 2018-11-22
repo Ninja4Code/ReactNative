6f85e44ec0a03d3c760471b6f520551302a3c386
Charles Owen <owen.charlesr@hotmail.com>
2018-11-21 22:00:28 -0500

    Redux in About

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,18 +3,26 @@ import { Image, FlatList,ScrollView,Text } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { LEADERS } from '../shared/leaders';
 import { Card } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+        leaders: state.leaders
+    }
+}
 
 function History(){
     return(
         <Card
-        title='Our History'>
-<Text style={{margin:10 }}>
-Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.
-</Text>
-<Text style = {{margin:10}}>
-The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.
-</Text>
-</Card>
+          title='Our History'>
+          <Text style={{margin:10 }}>
+            Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.
+          </Text>
+          <Text style = {{margin:10}}>
+           The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.
+          </Text>
+        </Card>
     )
 }
 function Renderleader({item, index})  {
@@ -33,13 +41,13 @@ function Renderleader({item, index})  {
 
 class About extends Component{
 
-constructor(props){
+/*constructor(props){
     super(props);
     this.state = {
         leaders:LEADERS
     };
   
-}
+}*/
 static navigationOptions ={
     title:"About Us"
     };
@@ -50,7 +58,7 @@ render(){
           <History/>
           <Card  title = 'Corporate LeaderShip'>
             <FlatList 
-                    data={this.state.leaders}
+                    data={this.props.leaders.leaders}
                     renderItem={Renderleader}
                     keyExtractor={item => item.id.toString()}
             />
@@ -59,4 +67,4 @@ render(){
     )   
 }
 }
-export default About;
+export default connect(mapStateToProps)(About);

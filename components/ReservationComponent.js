7ac38bffc3fb65a214bf8de5bc9f8418614848eb
Charles Owen <owen.charlesr@hotmail.com>
2018-12-05 10:15:23 -0500

    Animatable Reservation 1

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Modal } from 'react-native';
 import DatePicker from 'react-native-datepicker';
+import * as Animatable from 'react-native-animatable';
 
 class Reservation extends Component {
 

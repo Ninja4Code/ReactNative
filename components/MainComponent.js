28aba3e0141309017f2d918b8387a5738f751d34
Charles Owen <owen.charlesr@hotmail.com>
2018-12-17 21:12:54 -0500

    Network Info

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { View, Platform } from 'react-native';
 import { connect } from 'react-redux';
 import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
 import { MainNavigator} from './MyMenu';
+import { View, Platform, Text, ScrollView, Image, StyleSheet, NetInfo, ToastAndroid } from 'react-native';
 
 const mapStateToProps = state => {
   return {      
@@ -15,11 +15,42 @@ const mapDispatchToProps = dispatch => ({
   // fetchComments:() => dispatch(fetchComments())
 });
 class Main extends Component {
+  
   componentDidMount() {
     this.props.fetchLeaders();
    // this.props.fetchComments();
     this.props.fetchDishes();
     this.props.fetchPromos();
+
+    NetInfo.getConnectionInfo()
+        .then((connectionInfo) => {
+            ToastAndroid.show('Initial Network Connectivity Type: '
+                + connectionInfo.type + ', effectiveType: ' + connectionInfo.effectiveType,
+                ToastAndroid.LONG)
+        });
+
+    NetInfo.addEventListener('connectionChange', this.handleConnectivityChange);
+  }
+  componentWillUnmount() {
+    NetInfo.removeEventListener('connectionChange', this.handleConnectivityChange);
+  }
+  handleConnectivityChange = (connectionInfo) => {
+    switch (connectionInfo.type) {
+      case 'none':
+        ToastAndroid.show('You are now offline!', ToastAndroid.LONG);
+        break;
+      case 'wifi':
+        ToastAndroid.show('You are now connected to WiFi!', ToastAndroid.LONG);
+        break;
+      case 'cellular':
+        ToastAndroid.show('You are now connected to Cellular!', ToastAndroid.LONG);
+        break;
+      case 'unknown':
+        ToastAndroid.show('You now have unknown connection!', ToastAndroid.LONG);
+        break;
+      default:
+        break;
+    }
   }
   render() {
     return (       
